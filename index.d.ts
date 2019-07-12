ddbdf91d17d3576625b749efef1c99bf51c766d1
ANNotunzdY® (Takayoshi Katsuki) <dev@annotunzdy.info>
2019-07-12 18:35:21 +0900

    Add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+import { Client, SearchParams, NameList } from 'elasticsearch';
+import { Readable, ReadableOptions } from '@machinomy/types-readable-stream';
+
+declare function ElasticsearchScrollStream(client: Client, query_opts: SearchParams, optional_fields: NameList, stream_opts: ReadableOptions): LibElasticsearchScrollStream;
+declare class LibElasticsearchScrollStream　extends Readable {
+}
