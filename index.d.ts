164175fffb2b22de6e79799b4ff670ea2d5d5477
ANNotunzdY® (Takayoshi Katsuki) <dev@annotunzdy.info>
2019-07-12 20:37:05 +0900

    types-readable-stream import bug fix

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import { Client, SearchParams, NameList } from 'elasticsearch';
-import { Readable, ReadableOptions } from '@machinomy/types-readable-stream';
+import { Readable, ReadableOptions } from 'readable-stream';
 
 declare function ElasticsearchScrollStream(client: Client, query_opts: SearchParams, optional_fields: NameList, stream_opts: ReadableOptions): LibElasticsearchScrollStream;
 declare class LibElasticsearchScrollStream　extends Readable {
