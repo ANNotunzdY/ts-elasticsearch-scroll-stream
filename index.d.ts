173927cead777bfab3a676d15b048458fe5b369e
ANNotunzdY® (Takayoshi Katsuki) <dev@annotunzdy.info>
2019-07-17 20:19:59 +0900

    declare using class

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 import { Client, SearchParams, NameList } from 'elasticsearch';
 import { Readable, ReadableOptions } from 'readable-stream';
 
-declare function ElasticsearchScrollStream(client: Client, query_opts: SearchParams, optional_fields: NameList, stream_opts: ReadableOptions): LibElasticsearchScrollStream;
-declare class LibElasticsearchScrollStream　extends Readable {
+declare class ElasticsearchScrollStream extends Readable {
+    constructor(client: Client, query_opts: SearchParams, optional_fields: NameList, stream_opts: ReadableOptions);
 }
